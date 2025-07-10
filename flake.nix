f000dc602ae69fe06d70151a171069308d06d809
silvarc141 <silvarc141@users.noreply.github.com>
2025-07-10 18:16:33 +0200

    update utils

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     legacyPackages = genAttrs allSystems (system: import ./pkgs {
         pkgs = nixpkgs.legacyPackages.${system};
         pkgsSelf = self.legacyPackages.${system};
-        utils = nix-utils.legacyPackages.${system}.lib;
+        utils = nix-utils.legacyPackages.${system};
       });
     formatter = genAttrs allSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
     defaultPackage = genAttrs allSystems (system: self.legacyPackages.${system}.mk-ruby-squib-env);
