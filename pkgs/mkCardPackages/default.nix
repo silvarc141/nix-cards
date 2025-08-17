9721b31e55a41a136fb788e5de4e50af4562b016
silvarc141 <silvarc141@users.noreply.github.com>
2025-08-17 02:26:05 +0200

    fix omission

--- pkgs/mkCardPackages/default.nix
+++ pkgs/mkCardPackages/default.nix
@@ -2,6 +2,7 @@
   lib,
   runCommand,
   mkRubySquibEnv,
+  ...
 }: {
   cardTypes,
   graphicsDir,
--- pkgs/squib-environment/default.nix
+++ pkgs/squib-environment/default.nix
@@ -1,1 +1,1 @@
-{mk-ruby-squib-env, ...}: mk-ruby-squib-env []
+{mkRubySquibEnv, ...}: mkRubySquibEnv []
