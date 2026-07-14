7017eba932a0ea596ed0bed1b037bb2e775bb074
silvarc141 <silvarc141@users.noreply.github.com>
2026-07-14 16:29:53 +0200

    add ruby-squib-env test check

--- checks.nix
+++ checks.nix
@@ -1,4 +1,26 @@
-{ pkgs, pkgsSelf, ... }: {
+{
+  pkgs,
+  pkgsSelf,
+  ...
+}:
+{
+  test-ruby-squib-env =
+    let
+      squib-sample = pkgs.writeText "hello.rb" /* ruby */ ''
+        require 'squib'
+        Squib::Deck.new cards: 1 do
+          background color: 'pink'
+          rect
+          text str: 'Draw two cards.'
+          save_png prefix: 'part1_'
+        end
+      '';
+    in
+    pkgs.runCommand "build-squib-sample" { } ''
+      ${pkgs.lib.getExe pkgsSelf.ruby-squib-env} ${squib-sample}
+      [ -d _output ] || (echo "Error: _output directory not found"; exit 1)
+      cp -r _output $out
+    '';
   test-mk-vtt-game =
     let
       gameName = "testGame";
